6084851ee958e9ef0a92e59b1c2be554363cbc7f
nuonuo-888 <2720161028@qq.com>
2025-11-25 17:48:50 +0800

    Add EdgeOne adapter and ignore generated files

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@
 import { defineConfig } from "astro/config";
 
 import icon from "astro-icon";
+import edgeoneAdapter from "@edgeone/astro";
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: edgeoneAdapter(),
   integrations: [icon()],
   prefetch: true,
 });
